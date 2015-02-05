25be0b58a575974441088af326b3349fa716a2b0
sricharan chiruvolu <sricharanized@gmail.com>
2015-02-05 21:52:46 +0530

    started with save_scene function

--- helperfunctions.cpp
+++ helperfunctions.cpp
@@ -6,4 +6,15 @@ namespace raintrace
 	{
 		auto primitives = Scene::PrimitiveVector{};
 	}
+	
+	void save_scene(ImageData img_data, string filename){
+	  
+	  
+	  
+	  boost::gil::planar_rgb_view view = boost::gil::planar_rgb_view(img_data.getWidth(), img_data.getHeight(), img_data.getR(), img_data.getG(), img_data.getB(), img_data.getWidth());
+	  
+	  boost::gil::jpeg_write_view("scene.jpg", view);
+	  
+	}
+	
 }
--- helperfunctions.h
+++ helperfunctions.h
@@ -2,10 +2,14 @@
 #define __RAINTRACE_HELPERFUNCTIONS_H__
 
 #include <glm/glm.hpp>
+#include <boost/gil/extension/io/jpeg_io.hpp>
+
 
 #include "classes/Scene.h"
 #include "classes/Sphere.h"
 #include "classes/Plane.h"
+#include "classes/imageData.h"
+
 
 namespace raintrace
 {
@@ -13,6 +17,11 @@ namespace raintrace
 	 * Builds a scene with a sphere resting on a plane surface.
 	 */
 	Scene buildSphereScene();
+	
+	/*
+	 * Save the scene as a JPEG image.
+	 */
+	void save_scene();
 }
 
 #endif
