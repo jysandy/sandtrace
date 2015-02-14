293b7fd27249a469c96e253371777efe70ae48a1
Sandilya Jandhyala <jysandilya@gmail.com>
2015-02-14 19:45:29 +0530

    Added progress feedback and rendering time

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <string>
+#include <chrono>
 
 #include "classes/image_data.h"
 #include "classes/scene.h"
@@ -7,11 +7,14 @@
 int main(int argc, char** argv)
 {
 	using namespace sandtrace;
-	cout << "Rendering..." << endl;
-	clock_t t1, t2;
-	t1 = clock();
-	auto scene = build_sphere_scene();
+	using namespace std::chrono = chrono;
 
+	std::cout << "Constructing scene...";
+	auto scene = build_sphere_scene();
+	std::cout << "done." << std::endl;
+
+	std::cout << "Rendering...";
+	auto begin_time = chrono::steady_clock::now();
 	const int render_width = 400;
 	const int render_height = 400;
 
@@ -24,10 +27,19 @@ int main(int argc, char** argv)
 			im_data(i, j) = ray_traced_color(ray, scene);
 		}
 	}
+	auto duration = begin_time - chrono::steady_clock::now();
+	std::cout << "done." << std::endl;
+	auto hours = chrono::duration_cast<chrono::hours>(duration);
+	auto minutes = chrono::duration_cast<chrono::minutes>(duration % chrono::hours(1));
+	auto seconds = chrono::duration_cast<chrono::seconds>(duration % chrono::minutes(1));
+	std::cout << "Rendering time: ";
+	std::cout << std::setfill('0') << std::setw(2) << hours.count() << ":"
+									<< std::setw(2) << minutes.count() << ":"
+									<< std::setw(2) << seconds.count() << std::endl;
 
-	std::string filename = "scene.png";
-	save_scene(im_data, filename);
+	std::cout << "Saving to scene.jpg...";
+	save_scene(im_data, "scene.jpg");
+	std::cout << "done." << std::endl;
 
-	t2 = clock();
-	cout << ((float)t2 - (float)t1)/ 1000 << endl;
+	return 0;
 }
