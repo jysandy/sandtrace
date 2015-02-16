394475d5b27b9142de384af288e623ecda1db7ee
Sandilya Jandhyala <jysandilya@gmail.com>
2015-02-16 18:06:39 +0530

    Fixed rendering time computation

--- main.cpp
+++ main.cpp
@@ -28,14 +28,16 @@ int main(int argc, char** argv)
 			im_data(i, j) = ray_traced_color(pixel_ray, sphere_scene);
 		}
 	}
-	auto duration = begin_time - chrono::steady_clock::now();
+	auto duration = chrono::steady_clock::now() - begin_time;
 	std::cout << "done." << std::endl;
 
 	auto hours = chrono::duration_cast<chrono::hours>(duration);
 	auto minutes = chrono::duration_cast<chrono::minutes>(duration % chrono::hours(1));
 	auto seconds = chrono::duration_cast<chrono::seconds>(duration % chrono::minutes(1));
+	auto milliseconds = chrono::duration_cast<chrono::milliseconds>(duration % chrono::seconds(1));
 	std::cout << "Rendering time: ";
-	std::cout << hours.count() << ":" << minutes.count() << ":" << seconds.count() << std::endl;
+	std::cout << hours.count() << ":" << minutes.count() << ":" << seconds.count()
+		<< ":" << milliseconds.count() << std::endl;
 
 	std::cout << "Saving to scene.jpg...";
 	save_scene(im_data, "scene.jpg");
